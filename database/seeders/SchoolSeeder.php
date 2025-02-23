73acca3a14fcc44aba6828f99bdbad5d3916faea
Suzana Janeva <suzana@codeart.mk>
2025-02-23 13:10:03 +0100

    added user and school seeders

---
+++ database/seeders/SchoolSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\School;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+
+class SchoolSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('schools')->insert([
+            'name' => Str::random(10),
+            'slug'=> Str::random(5),
+            'domain'=>  Str::random(5),
+            'logo' => null,
+        ]);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\School;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@yahoo.com',
+            'password' => Hash::make('admin'),
+            'role' => 'admin',
+            'school_id' => School::where('id', 1)->first(),
+        ]);
+    }
+}
